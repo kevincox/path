90becd07bb534794fc78ee0f6eca75d6844900b6
Kevin Cox <kevincox@kevincox.ca>
2013-10-21 21:40:10 -0400

    Basic stuff.

---
+++ path.c
@@ -0,0 +1,11 @@
+// Copyright 2013 Kevin Cox
+
+/*
+ * I appologize for having no functions, this program is simple and should
+ * run through once in a linear fashion.  Functions aren't really useful.
+ */
+
+int main (int argc, char **argv)
+{
+
+}
